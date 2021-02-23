c4d545b6b304be44d5961a9fda52a792074405f1
Dariusz Gawron <screbsy+g1@gmail.com>
2021-02-23 21:59:12 +0100

    Add product gallery

--- client/src/Components/PictureView/PictureView.js
+++ client/src/Components/PictureView/PictureView.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './PictureView.css';
 
 const PictureView = (props) => {
-  // const grafiki = props.grafiki.filter(photo=>photo.typ_grafiki!==0);
+  const images = props.images;
+  const imageTitle = props.title;
+  let pictureNumber = 0;
   let id = props.pictureNumber;
+  let pictureIndex = 1;
   // btnOnClick (e) {
   //   const productsList = document.getElementsByClassName('productsList');
   //   (e.target.classList[1]==='productsNav--left') 
@@ -15,34 +18,61 @@ const PictureView = (props) => {
   // }
 
   const nextPicture = () => {
-
+    pictureIndex++;
+    displayPicture(pictureIndex);
   }
 
-  const previousPicture = () => {
+  const prevPicture = () => {
+    pictureIndex--;
+    displayPicture(pictureIndex);
+  }
 
+  const displayPicture = (index) => {
+    let pictures = document.getElementsByClassName('pictureView__image');
+    let caption = document.getElementsByClassName('pictureView__caption');
+    if (index > images.length)
+      index = 1;
+    else if (index < 1)
+      index = images.length;
+    let pictureIndex;
+    for (pictureIndex = 0; pictureIndex < images.length; pictureIndex++ ) {
+      pictures[pictureIndex].style.display = "none";
+    }
+    pictures[pictureIndex-1].style.display = "block";
+  }
+
+  const openPicture = (index) => {
+    document.getElementsByClassName('pictureView')[0].style.display = "block";
+    pictureIndex = index;
+    displayPicture(pictureIndex);
   }
 
   const closePicture = () => {
-
+    ducument.getElementsByClassName('pictureView')[0].style.display = "none";
   }
 
   return (
     <div className="pictureView">
-      <div className="pictureView__header">
-      
-      </div>
+      <span className="pictureView__close" onClick="closePicture()">&times;</span>
       <div className="pictureView__content">
-        <div className="pictureView__container">
-          <button className="pictureView__button pictureView__button--left">
-            <i className="fas fa-chevron-left"></i>
-          </button>
-          <div className="pictureView__image">
-            
-          </div>
-          <button className="pictureView__button pictureView__button--right">
-            <i className="fas fa-chevron-right"></i>
-          </button>
-        </div>
+        {images.map(image => {
+          return (
+            <div className="pictureView__image">
+              <img src={image} alt={`${imageTitle} (${++pictureNumber})`} />
+            </div>
+          )
+        })}
+        
+        
+
+        <button className="pictureView__button pictureView__button--prev" onClick="nextPicture()">
+          &#10094;
+        </button>
+        <button className="pictureView__button pictureView__button--next" onClick="prevPicture()">
+          &#10095;
+        </button>
+
+        <div className="pictureView__caption"></div>
       </div>
     </div>
   )
