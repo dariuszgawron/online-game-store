e719873d5ad8a75d09860550b9b19dcfd3038ba5
Dariusz Gawron <screbsy+g1@gmail.com>
2021-02-24 22:04:34 +0100

    Add pictureview component

--- client/src/Components/PictureView/PictureView.js
+++ client/src/Components/PictureView/PictureView.js
@@ -5,17 +5,9 @@ const PictureView = (props) => {
   const images = props.images;
   const imageTitle = props.title;
   let pictureNumber = 0;
-  let id = props.pictureNumber;
+  // let id = props.pictureNumber;
   let pictureIndex = 1;
-  // btnOnClick (e) {
-  //   const productsList = document.getElementsByClassName('productsList');
-  //   (e.target.classList[1]==='productsNav--left') 
-  //   ? productsList[1].scrollLeft -= (window.innerWidth/2)
-  //   : productsList[1].scrollLeft += (window.innerWidth/2);
-  //   // console.log(productsList[1].scrollWidth - productsList[1].clientWidth);
-  //   console.log(productsList[1].scrollWidth - productsList[1].scrollLeft);
-  //   // this.props.addProduct({'title':'adaf','price': 12,'amount':1,'productId':1});
-  // }
+  // displayPicture(pictureIndex);
 
   const nextPicture = () => {
     pictureIndex++;
@@ -28,32 +20,38 @@ const PictureView = (props) => {
   }
 
   const displayPicture = (index) => {
-    let pictures = document.getElementsByClassName('pictureView__image');
-    let caption = document.getElementsByClassName('pictureView__caption');
-    if (index > images.length)
-      index = 1;
-    else if (index < 1)
-      index = images.length;
-    let pictureIndex;
-    for (pictureIndex = 0; pictureIndex < images.length; pictureIndex++ ) {
-      pictures[pictureIndex].style.display = "none";
+    if (document.getElementsByClassName('pictureView__image')[0] && 
+        document.getElementsByClassName('pictureView__caption')[0])
+    {
+      let pictures = document.getElementsByClassName('pictureView__image');
+      let caption = document.getElementsByClassName('pictureView__caption');
+      if (index > images.length)
+        index = 1;
+      else if (index < 1)
+        index = images.length;
+      let pictureIndex;
+      for (pictureIndex = 0; pictureIndex < images.length; pictureIndex++ ) {
+        pictures[pictureIndex].style.display = "none";
+      }
+      pictures[pictureIndex-1].style.display = "block";
+      caption.innerHTML = `${imageTitle} +(${pictureIndex})`;
     }
-    pictures[pictureIndex-1].style.display = "block";
   }
 
-  const openPicture = (index) => {
-    document.getElementsByClassName('pictureView')[0].style.display = "block";
-    pictureIndex = index;
-    displayPicture(pictureIndex);
-  }
+  // const openPicture = (index) => {
+  //   document.getElementsByClassName('pictureView')[0].style.display = "block";
+  //   pictureIndex = index;
+  //   displayPicture(pictureIndex);
+  // }
 
   const closePicture = () => {
-    ducument.getElementsByClassName('pictureView')[0].style.display = "none";
+    if(document.getElementById('pictureView'))
+      document.getElementById('pictureView').style.display = "none";
   }
 
   return (
-    <div className="pictureView">
-      <span className="pictureView__close" onClick="closePicture()">&times;</span>
+    <div className="pictureView" id="pictureView">
+      <span className="pictureView__close" onClick={closePicture()}>&times;</span>
       <div className="pictureView__content">
         {images.map(image => {
           return (
@@ -62,13 +60,11 @@ const PictureView = (props) => {
             </div>
           )
         })}
-        
-        
 
-        <button className="pictureView__button pictureView__button--prev" onClick="nextPicture()">
+        <button className="pictureView__button pictureView__button--prev" onClick={nextPicture()}>
           &#10094;
         </button>
-        <button className="pictureView__button pictureView__button--next" onClick="prevPicture()">
+        <button className="pictureView__button pictureView__button--next" onClick={prevPicture()}>
           &#10095;
         </button>
 
--- client/src/Components/ProductGallery/ProductGallery.js
+++ client/src/Components/ProductGallery/ProductGallery.js
@@ -8,6 +8,9 @@ class ProductGallery extends React.Component {
     this.state = {
       isLoaded: false
     };
+    this.images = this.props.grafiki.filter(photo=>photo.typ_grafiki!==0);
+    this.productTitle = /*this.props.title*/ 'Prince of Persia';
+    this.pictureIndex = 1;
     // this.btnOnClick = this.btnOnClick.bind(this);
   }
 
@@ -20,8 +23,47 @@ class ProductGallery extends React.Component {
   //   console.log(productsList[1].scrollWidth - productsList[1].scrollLeft);
   //   // this.props.addProduct({'title':'adaf','price': 12,'amount':1,'productId':1});
   // }
-  openImage() {
+  
 
+  nextPicture = () => {
+    this.pictureIndex++;
+    this.displayPicture(this.pictureIndex);
+  }
+
+  prevPicture = () => {
+    this.pictureIndex--;
+    this.displayPicture(this.pictureIndex);
+  }
+
+  displayPicture = (index) => {
+    if (document.getElementsByClassName('pictureView__image')[0] && 
+        document.getElementsByClassName('pictureView__caption')[0])
+    {
+      let pictures = document.getElementsByClassName('pictureView__image');
+      let caption = document.getElementsByClassName('pictureView__caption');
+      if (index > this.images.length)
+        index = 1;
+      else if (index < 1)
+        index = this.images.length;
+      for (let index = 0; index < this.images.length; index++ ) {
+        pictures[index].style.display = "none";
+      }
+      pictures[this.pictureIndex-1].style.display = "block";
+      caption.innerHTML = `${this.productTitle} +(${this.pictureIndex})`;
+    }
+  }
+
+  openPicture = (index=2) => {
+    if (document.getElementsByClassName('pictureView')[0]) {
+      document.getElementsByClassName('pictureView')[0].style.display = "block";
+      this.pictureIndex = index;
+      this.displayPicture(this.pictureIndex);
+    }
+  }
+
+  closePicture = () => {
+    if(document.getElementById('pictureView'))
+      document.getElementById('pictureView').style.display = "none";
   }
 
   render() {
@@ -29,10 +71,14 @@ class ProductGallery extends React.Component {
     const zwiastuny = this.props.zwiastuny;
     return (
       <div className="productGallery tab" id="Gallery">
-        {/* <PictureView images={grafiki} /> */}
+        <PictureView images={grafiki} title='Prince' />
         {/* <div className="gallery"> */}
-          {grafiki.map(photo=>{
-            return <img src={photo.sciezka_do_pliku} />
+          {grafiki.map((photo,index) => {
+            return <img src = {photo.sciezka_do_pliku} alt={`${this.productTitle} (${index+1})`} 
+                        index = {index} onClick = {this.openPicture()} 
+                        closePicture = {this.closePicture} 
+                        displayPicture = {this.displayPicture} 
+                    />
           })}
           {zwiastuny.map(zwiastun => {
             return <iframe width="100%" height="200px" src="https://www.youtube.com/embed/LXK905yxHQg" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowFullScreen></iframe>
